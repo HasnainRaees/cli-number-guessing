89068ac79b6278f0669fc05c53e178694f23dd76
hasnain raees <hasnainraees07@gmail.com>
2024-03-30 02:45:31 +0500

    push my code

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userNumber",
+        type: "number",
+        message: "Enter your guess number between 1-10:",
+    },
+]);
+if (answer.userNumber === randomNumber) {
+    console.log(chalk.blue("Congratulation ! you guess a right number"));
+}
+else {
+    console.log(chalk.red("Try again. Better luck next time"));
+}
